d502926199056fb53643ec0a5f9bd75c53c8720d
jackturner <jack@jackturner.design>
2019-11-29 16:13:41 -0500

    sitemaps, google verification, repo cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    ejs = require('gulp-ejs')
+    ejs = require('gulp-ejs'),
+    runSequence = require('run-sequence'),
+    sitemap = require('gulp-sitemap')
 
 gulp.task('sass', function () {
   return gulp.src('src/styles/**/*.scss')
@@ -29,6 +31,21 @@ gulp.task('videos',function(){
     .pipe(gulp.dest('docs/videos/'))
 })
 
+gulp.task('move-google-verification', function(){  
+  return gulp.src('google8e356c7d3c1d1496.html')
+    .pipe(gulp.dest('docs/'))
+})
+
+gulp.task('sitemap', function () {
+  gulp.src('docs/**/*.html', {
+          read: false
+      })
+      .pipe(sitemap({
+          siteUrl: 'https://hap.video'
+      }))
+      .pipe(gulp.dest('./docs'))
+})
+
 gulp.task('watch', function() {  
     gulp.watch('src/styles/**/*.scss', ['sass'])
     gulp.watch('src/js/**/*.js', ['scripts'])
@@ -37,6 +54,8 @@ gulp.task('watch', function() {
     gulp.watch('src/videos/**/*.*', ['videos'])
 })
 
-gulp.task('build', ['sass', 'scripts', 'ejs', 'images', 'videos'])
+gulp.task('build', ['sass', 'scripts', 'ejs', 'images', 'videos', 'move-google-verification'], function (cb) {
+  runSequence(['sitemap'], cb);
+})
 
 gulp.task('default', ['build', 'watch'])
